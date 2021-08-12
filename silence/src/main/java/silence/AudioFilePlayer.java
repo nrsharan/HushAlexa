73da323567e7baf55c05cb55dfa051dcd95ab004
nrsharan <sharan.nuraniramesh@ruhr-uni-bochum.de>
2021-08-12 19:26:50 +0200

    Initial Commit

---
+++ silence/src/main/java/silence/AudioFilePlayer.java
@@ -0,0 +1,82 @@
+package silence;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine.Info;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import static javax.sound.sampled.AudioSystem.getAudioInputStream;
+import static javax.sound.sampled.AudioFormat.Encoding.PCM_SIGNED;
+
+public class AudioFilePlayer {
+
+	public static void main(String[] args) throws URISyntaxException {
+		final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+		executorService.scheduleAtFixedRate(() -> {
+			try {
+				play();
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			}
+		}, 0, 300, TimeUnit.SECONDS);
+	}
+
+	public static void play() throws URISyntaxException {
+
+		// final File file = new File( "C:\\Users\\nrsha\\Research
+		// Workspaces\\Java643\\silence\\src\\main\\resources\\sounds\\ding.mp3");
+
+		// System.out.println(Thread.currentThread().getContextClassLoader().getResource(""));
+		// System.out.println(getClass().getResource(""));
+		// final File file = new File("src\\main\\resources\\sounds\\ding.mp3");
+		// final File file = new
+		// File(Thread.currentThread().getContextClassLoader().getResource("") +
+		// "/sounds/ding.mp3");
+		// System.out.println(Thread.currentThread().getContextClassLoader().getResource(""));
+		// ClassLoader cl = AudioFilePlayer.class.getClassLoader();
+		// System.out.println(cl.getResource("ding.mp3"));
+		try (final AudioInputStream in = getAudioInputStream(
+				Thread.currentThread().getContextClassLoader().getResource("quiet.mp3"))) {
+
+			final AudioFormat outFormat = getOutFormat(in.getFormat());
+			final Info info = new Info(SourceDataLine.class, outFormat);
+
+			try (final SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info)) {
+
+				if (line != null) {
+					line.open(outFormat);
+					line.start();
+					stream(getAudioInputStream(outFormat, in), line);
+					line.drain();
+					line.stop();
+				}
+			}
+
+		} catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	private static AudioFormat getOutFormat(AudioFormat inFormat) {
+		final int ch = inFormat.getChannels();
+
+		final float rate = inFormat.getSampleRate();
+		return new AudioFormat(PCM_SIGNED, rate, 16, ch, ch * 2, rate, false);
+	}
+
+	private static void stream(AudioInputStream in, SourceDataLine line) throws IOException {
+		final byte[] buffer = new byte[4096];
+		for (int n = 0; n != -1; n = in.read(buffer, 0, buffer.length)) {
+			line.write(buffer, 0, n);
+		}
+	}
+}
